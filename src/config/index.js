e04f14fbf8967537dad85626a6b00412444fc514
Hanwen Cheng <heawen.cheng@gmail.com>
2020-08-27 01:59:38 +0200

    add pubsub and re-styling

--- src/config/index.js
+++ src/config/index.js
@@ -3,9 +3,15 @@ const config = {
     preload: {
       enabled: false
     },
+    EXPERIMENTAL: {
+      pubsub: true // required, enables pubsub
+    },
     config: {
       Addresses: {
-        Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star']
+        Swarm: [
+          // '/dns4/damp-lake-31712.herokuapp.com/tcp/443/wss/p2p-webrtc-star/'
+          '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+        ]
       }
     }
   }
